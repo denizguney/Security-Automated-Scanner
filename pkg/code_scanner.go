e22e24e0aa6400e853a0d4da1264a588ea947824
Deniz Güney Yıldırım <31777283+denizguney@users.noreply.github.com>
2026-07-19 00:24:46 +0300

    Update code_scanner.go

--- pkg/code_scanner.go
+++ pkg/code_scanner.go
@@ -5,3 +5,44 @@ import(
   "path/filepath"
   "strings"
 )
+
+//CodeScanner yapısı belirli bir dizini tarar
+
+type CodeScanner struct{
+
+  TargetDir string
+}
+
+//Scan hedef dizinideki dosyaları gezer
+
+func(s *CodeScanner) Scan(){
+
+  TargetDir string
+}
+
+//Scan hedef dizindeki dosyaları gezer
+func(s *CodeScanner) Scan(){
+
+  filepath.WalkDir(s.TargetDir, func(path string, d fs.DirEntry, err error) error{
+
+    if err != nil{
+
+      return err
+  }
+
+    //Kapsam dışı dizinleri atla (Kurallara uyum [span_0](start_span)[span_0](end_span)
+
+    if strings.Contains(path, "samples") || strings.Contains(path, "tutorials") || string.Contains(path, "demo")
+    return nil
+}
+
+                   //Sadece .py ve .cs dosyalarını incele 
+
+                   if !id.IsDir() && (strings.HasSuffix(path, ".py") || strings.HasSuffix(path, ".cs")){
+
+                     //Burada dosya içeriğini okuyup analiz edeceğiz 
+                     //İleride buraya zafiyet kalıplarımızı (B seçeneği) ekleyeceğiz
+                   }
+                   return nil
+                   })
+  }
